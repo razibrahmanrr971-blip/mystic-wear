87717ff6c8ed8f856666b91212a922ad3ecb777b
razibrahmanrr971-blip <razibrahmanrr971@gmail.com>
2026-06-04 18:26:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+let cartCount = 0;
 document.querySelectorAll(".product-card button").forEach(btn => {
     btn.addEventListener("click", () => {
+        cartCount++;
+        localStorage.setItem("cartCount", cartCount);
         alert("Product added to cart!");
     });
 });
-console.log("Mystic Wear Loaded Successfully");
+window.onload = () => {
+    const savedCount = localStorage.getItem("cartCount");
+    if(savedCount){
+        cartCount = savedCount;
+    }
+    console.log("Items in Cart:", cartCount);
+};
